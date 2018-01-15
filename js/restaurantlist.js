d948ec9f9b0889c520432b53989414e1aa6e8b16
subashnigajendran <subaappu@gmail.com>
2018-01-14 22:33:17 -0500

    [NEW] - search page

---
+++ js/restaurantlist.js
@@ -0,0 +1,31 @@
+var app = angular.module("sampleapp", []);
+app.controller('samplecontoller', ['$scope', '$http', function ($scope, $http) { 
+  $scope.searchRestaurant = function(value) {
+  $scope.datalists = '';
+  $http.defaults.headers.common['Accept-Tenant'] ='uk';
+  $http.defaults.headers.common['Accept-Language'] ='en-GB';
+  $http.defaults.headers.common.Authorization ='Basic VGVjaFRlc3RBUEk6dXNlcjI=';
+  $http.get("http://public.je-apis.com/restaurants?q="+value)
+    .then(function(response){
+        $scope.datalists = response.data.Restaurants;
+        $scope.curPage = 0;
+        $scope.pageSize = 5;
+        $scope.datalists = response.data.Restaurants;
+
+      //console.log($scope.datalists.length);
+
+        $scope.numberOfPages = function() {
+        return Math.ceil($scope.datalists.length / $scope.pageSize);
+      }
+    });
+  }
+}]);
+
+angular.module('sampleapp').filter('pagination', function()
+{
+  return function(input, start)
+  {
+    start = +start;
+    return input.slice(start);
+  };
+})
